1be6614efee4e4486b106ed31c2127689b14fecd
David Catmull <catmull@google.com>
2011-08-04 16:35:38 -0600

    NSData methods aren't relevant.
    Also some formatting cleanup.

--- Xit/Xit.m
+++ Xit/Xit.m
@@ -13,7 +13,7 @@
 
 @synthesize selectedCommit;
 
-- (id)init
+-(id)init
 {
     self = [super init];
     if (self) {
@@ -27,7 +27,7 @@
     return self;
 }
 
-- (id)initWithContentsOfURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError
+-(id)initWithContentsOfURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError
 {
     absoluteURL=[absoluteURL URLByDeletingPathExtension];
     self = [super initWithContentsOfURL:absoluteURL ofType:typeName error:outError];
@@ -55,14 +55,12 @@
     FSEventStreamInvalidate(stream);
 }
 
-- (NSString *)windowNibName
+-(NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"Xit";
 }
 
-- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+-(void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
     [super windowControllerDidLoadNib:aController];
     [sideBarDS setRepo:self];
@@ -73,29 +71,7 @@
     [self start];
 }
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
-    /*
-     Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-     You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-     */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return nil;
-}
-
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
-    /*
-     Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-     You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-     */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return YES;
-}
-
-- (BOOL)readFromURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError {
+-(BOOL)readFromURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError {
     return true; // XXX
 }
 
@@ -111,12 +87,12 @@
     NSLog(@"****command = git %@",[args componentsJoinedByString:@" "]);
     NSTask* task = [[NSTask alloc] init];
     [task setCurrentDirectoryPath:[repoURL path]];
-	[task setLaunchPath:gitCMD];
-	[task setArguments:args];
+    [task setLaunchPath:gitCMD];
+    [task setArguments:args];
     
-	NSPipe* pipe = [NSPipe pipe];
-	[task setStandardOutput:pipe];
-	[task setStandardError:pipe];
+    NSPipe* pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+    [task setStandardError:pipe];
     
     [task  launch];
     NSMutableData *output=[NSMutableData data];
@@ -163,12 +139,12 @@
     NSLog(@"****command = git %@",[args componentsJoinedByString:@" "]);
     NSTask* task = [[NSTask alloc] init];
     [task setCurrentDirectoryPath:[repoURL path]];
-	[task setLaunchPath:gitCMD];
-	[task setArguments:args];
+    [task setLaunchPath:gitCMD];
+    [task setArguments:args];
     
-	NSPipe* pipe = [NSPipe pipe];
-	[task setStandardOutput:pipe];
-	[task setStandardError:pipe];
+    NSPipe* pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+    [task setStandardError:pipe];
     
     [task  launch];
     NSData *output=[[pipe fileHandleForReading] readDataToEndOfFile];
@@ -203,7 +179,7 @@
                                  &fsevents_callback,
                                  &context,
                                  (CFArrayRef) pathsToWatch,
-	                             kFSEventStreamEventIdSinceNow,
+                                 kFSEventStreamEventIdSinceNow,
                                  (CFAbsoluteTime) latency,
                                  kFSEventStreamCreateFlagUseCFTypes
                                  );
