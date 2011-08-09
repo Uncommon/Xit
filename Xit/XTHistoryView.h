6dedbb2bbd2e679d8c687dc5a59bd81aedc632e1
German Laullon <glaullon@vmware.com>
2011-08-09 12:50:50 -0700

    Revert "stickyRow"
    This reverts commit b2200a9112e45bc3aa4d90be27b34591c4930929.

--- Xit/XTHistoryView.h
+++ Xit/XTHistoryView.h
@@ -21,15 +21,12 @@
     IBOutlet NSView *commitView;
     IBOutlet NSSplitView *sidebarSplitView;
     IBOutlet NSSplitView *mainSplitView;
-    IBOutlet NSTableView *table;
     @private
     Xit *repo;
-    NSTextField *stickyRow;
 }
 
 - (void)setRepo:(Xit *)newRepo;
 - (void)viewDidLoad;
--(void)tableChanges:(NSNotification*)aNotification;
 
 -(IBAction)toggleLayout:(id)sender;
 -(IBAction)toggleSideBar:(id)sender;
