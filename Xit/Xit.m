0de4db6c54e1c4c92c5b69adc440084eed815337
David Catmull <uncommon@uncommonplace.com>
2011-08-19 10:58:32 -0600

    cleanup

--- Xit/Xit.m
+++ Xit/Xit.m
@@ -40,11 +40,10 @@
 }
 
 - (id) initWithContentsOfURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError {
-    absoluteURL = [absoluteURL URLByDeletingPathExtension];
     self = [super initWithContentsOfURL:absoluteURL ofType:typeName error:outError];
     if (self) {
+		absoluteURL = [absoluteURL URLByDeletingPathExtension];
         repoURL = absoluteURL;
-        gitCMD = [Xit gitPath];
     }
     return self;
 }
@@ -64,8 +63,6 @@
 }
 
 - (NSString *) windowNibName {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"Xit";
 }
 
@@ -85,28 +82,6 @@
     [stageView setRepo:self];
 
     [self start];
-}
-
-- (NSData *) dataOfType:(NSString *)typeName error:(NSError **)outError {
-    /*
-     * Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-     * You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-     */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return nil;
-}
-
-- (BOOL) readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
-    /*
-     * Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-     * You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-     */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return YES;
 }
 
 - (BOOL) readFromURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError {
