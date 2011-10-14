192dd01034e3add1b0e7a419f166a43646e7eec9
David Catmull <uncommon@uncommonplace.com>
2011-10-14 13:08:19 -0600

    fix open dialog to open git folders

---
+++ Xit/XTAppDelegate.m
@@ -0,0 +1,50 @@
+//
+//  XTAppDelegate.m
+//  Xit
+//
+//  Created by David Catmull on 10/13/11.
+//
+
+#import "XTAppDelegate.h"
+#import "XTDocument.h"
+
+@implementation XTAppDelegate
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+
+    return self;
+}
+
+- (void)openDocument:(id)sender {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+
+    [panel setCanChooseFiles:NO];
+    [panel setCanChooseDirectories:YES];
+    [panel setDelegate:self];
+    [panel beginWithCompletionHandler:^(NSInteger result) {
+        if (result == NSFileHandlingPanelOKButton) {
+            for (NSURL *url in [panel URLs]) {
+                [[NSDocumentController sharedDocumentController] openDocumentWithContentsOfURL:url display:YES completionHandler:NULL];
+            }
+        }
+    }];
+}
+
+- (BOOL)panel:(id)sender validateURL:(NSURL *)url error:(NSError **)outError {
+    NSURL *repoURL = [url URLByAppendingPathComponent:@".git" isDirectory:YES];
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[repoURL path]])
+        return YES;
+    else {
+        NSAlert *alert = [NSAlert alertWithMessageText:@"That folder does not contain a Git repository." defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+
+        [alert beginSheetModalForWindow:[sender window] modalDelegate:nil didEndSelector:NULL contextInfo:NULL];
+        return NO;
+    }
+}
+
+@end
