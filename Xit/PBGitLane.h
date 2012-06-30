ecdf1b19b3dfdd8b83300b27292cfaad05e29659
David Catmull <uncommon@uncommonplace.com>
2012-06-29 23:25:04 -0600

    remove __MyCompanyName__ copyrights from GitX files

--- Xit/PBGitLane.h
+++ Xit/PBGitLane.h
@@ -3,7 +3,6 @@
 //  GitX
 //
 //  Created by Pieter de Bie on 27-08-08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 #import <Cocoa/Cocoa.h>
 
--- Xit/PBGitRevisionCell.m
+++ Xit/PBGitRevisionCell.m
@@ -3,7 +3,6 @@
 //  GitX
 //
 //  Created by Pieter de Bie on 17-06-08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
 #import "PBGitRevisionCell.h"
