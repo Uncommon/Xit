3a6ffd12a1555cc671ad6772f417ea3f1c28091b
David Catmull <uncommon@uncommonplace.com>
2013-11-22 13:40:43 -0700

    Delete moved diff methods

--- Xit/XTTextPreviewController.m
+++ Xit/XTTextPreviewController.m
@@ -33,59 +33,6 @@
   [[_webView mainFrame] loadHTMLString:html baseURL:baseURL];
 }
 
-+ (void)appendDiffLine:(NSString*)text
-                    to:(NSMutableString*)lines
-               oldLine:(NSUInteger)oldLine
-               newLine:(NSUInteger)newLine
-{
-  NSString *numberFormat = @"%lu";
-  NSString *class = @"pln";
-  NSString *oldLineText = @"";
-  NSString *newLineText = @"";
-
-  if (oldLine == -1)
-    class = @"add";
-  else
-    oldLineText =
-        [NSString stringWithFormat:numberFormat, (unsigned long)oldLine];
-  if (newLine == -1)
-    class = @"del";
-  else
-    newLineText =
-        [NSString stringWithFormat:numberFormat, (unsigned long)newLine];
-  [lines appendFormat:
-      @"<div class='%@'>"
-       "<span class='old'>%@</span>"
-       "<span class='new'>%@</span>"
-       "<span class='text'>%@</span>"
-       "</div>\n",
-       class, oldLineText, newLineText,
-       [[self class] escapeText:text]];
-}
-
-- (void)loadDiff:(XTDiffDelta*)delta
-{
-  NSString *htmlTemplate = [[self class] htmlTemplate:@"diff"];
-  NSMutableString *textLines = [NSMutableString string];
-
-  [delta enumerateHunksWithBlock:^(GTDiffHunk *hunk, BOOL *stop) {
-    [textLines appendString:@"<div class='hunk'>\n"];
-    [hunk enumerateLinesInHunkUsingBlock:^(GTDiffLine *line, BOOL *stop) {
-      [[self class] appendDiffLine:line.content
-                                to:textLines
-                           oldLine:line.oldLineNumber
-                           newLine:line.newLineNumber];
-    }];
-    [textLines appendString:@"</div>\n"];
-  }];
-
-  NSString *html = [NSString stringWithFormat:htmlTemplate, textLines];
-  NSURL *baseURL = [[NSBundle mainBundle]
-      URLForResource:@"html" withExtension:nil];
-
-  [[_webView mainFrame] loadHTMLString:html baseURL:baseURL];
-}
-
 - (BOOL)isFileChanged:(NSString*)path inRepository:(XTRepository*)repo
 {
   NSArray *changes = [repo changesForRef:repo.selectedCommit parent:nil];
@@ -100,30 +47,19 @@
           commit:(NSString*)sha
       repository:(XTRepository*)repository
 {
-  if ([self isFileChanged:path inRepository:repository]) {
-    XTDiffDelta *delta =
-        [repository diffForFile:path commitSHA:sha parentSHA:nil];
+  NSData *data = [repository contentsOfFile:path atCommit:sha];
+  NSString *text = [[NSString alloc]
+      initWithData:data encoding:NSUTF8StringEncoding];
 
-    if (delta != nil) {
-      [self loadDiff:delta];
-      return YES;
-    }
-    return NO;
-  } else {
-    NSData *data = [repository contentsOfFile:path atCommit:sha];
-    NSString *text = [[NSString alloc]
-        initWithData:data encoding:NSUTF8StringEncoding];
-
-    // TODO: Use TECSniffTextEncoding to detect encoding.
-    if (text == nil) {
-      text = [[NSString alloc]
-          initWithData:data encoding:NSUTF16StringEncoding];
-      if (text == nil)
-        return NO;
-    }
-    [self loadText:text];
-    return YES;
+  // TODO: Use TECSniffTextEncoding to detect encoding.
+  if (text == nil) {
+    text = [[NSString alloc]
+        initWithData:data encoding:NSUTF16StringEncoding];
+    if (text == nil)
+      return NO;
   }
+  [self loadText:text];
+  return YES;
 }
 
 @end
