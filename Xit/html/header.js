cd48c504f555e92e0e17a4191319795a3d1d9601
David Catmull <uncommon@uncommonplace.com>
2014-05-14 12:52:06 -0600

    js/css fixup

--- Xit/html/header.js
+++ Xit/html/header.js
@@ -8,13 +8,13 @@ function disclosure(clicked)
 	var button = document.getElementById("triangle");
 	var hidingIds = ['committer', 'sha', 'parents'];
 	var newDisplay = 'none';
-	var newImage = 'undisclosed.png'
+	var newImage = 'undisclosed.png';
 
 	if (isCollapsed()) {
 		newDisplay = 'block';
 		newImage = 'disclosed.png'
 	}
-	for (index in hidingIds) {
+	for (var index in hidingIds) {
 		document.getElementById(hidingIds[index]).style.display = newDisplay;
 	}
 	button.src = newImage;
