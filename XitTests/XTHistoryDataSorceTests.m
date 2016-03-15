d70addb84554aa415e5bf11fb29d919b3567fffb
David Catmull <uncommon@uncommonplace.com>
2016-03-15 15:40:17 -0600

    clarify testRootCommitsGraph

--- XitTests/XTHistoryDataSorceTests.m
+++ XitTests/XTHistoryDataSorceTests.m
@@ -23,53 +23,57 @@
 
 - (void)testRootCommitsGraph
 {
-  NSInteger nCommits = 15;
-  NSFileManager *defaultManager = [NSFileManager defaultManager];
+  const NSInteger nCommits = 15;
+  NSFileManager *fileManager = [NSFileManager defaultManager];
 
   for (int n = 0; n < nCommits; n++) {
-    NSString *rn = [NSString stringWithFormat:@"refs/heads/root_%d", n];
     if ((n % 5) == 0) {
-      NSData *data =
-          [repository executeGitWithArgs:@[ @"symbolic-ref", @"HEAD", rn ]
-                                  writes:NO
-                                   error:nil];
-      if (data == nil) {
-        XCTFail(@"'%@' error", rn);
-      }
+      // Every 5th commit, create a new root commit
+      NSString *rootName = [NSString stringWithFormat:@"refs/heads/root_%d", n];
+      NSData *data;
+      
+      data = [repository executeGitWithArgs:@[ @"symbolic-ref",
+                                               @"HEAD", rootName ]
+                                     writes:NO
+                                      error:nil];
+      if (data == nil)
+        XCTFail(@"'%@' error", rootName);
+      
+      // Recursively unstage the current directory
       data = [repository executeGitWithArgs:@[ @"rm", @"--cached", @"-r", @"." ]
                                      writes:NO
                                       error:nil];
-      if (data == nil) {
-        XCTFail(@"'%@' error", rn);
-      }
+      if (data == nil)
+        XCTFail(@"'%@' error", rootName);
+      
+      // Delete all untracked files
       data = [repository executeGitWithArgs:@[ @"clean", @"-f", @"-d" ]
                                      writes:NO
                                       error:nil];
-      if (data == nil) {
-        XCTFail(@"'%@' error", rn);
-      }
+      if (data == nil)
+        XCTFail(@"'%@' error", rootName);
     }
 
-    NSString *testFile =
+    NSString *testFilePath =
         [NSString stringWithFormat:@"%@/file%d.txt", repoPath, n];
     NSString *txt = [NSString stringWithFormat:@"some text %d", n];
-    [txt writeToFile:testFile
+    [txt writeToFile:testFilePath
           atomically:YES
             encoding:NSASCIIStringEncoding
                error:nil];
 
-    if (![defaultManager fileExistsAtPath:testFile]) {
+    if (![fileManager fileExistsAtPath:testFilePath]) {
       XCTFail(@"testFile NOT Found!!");
     }
-    if (![repository stageFile:[testFile lastPathComponent]]) {
-      XCTFail(@"add file '%@'", testFile);
+    if (![repository stageFile:[testFilePath lastPathComponent]]) {
+      XCTFail(@"add file '%@'", testFilePath);
     }
     if (![repository commitWithMessage:[NSString stringWithFormat:@"new %@",
-                                                                  testFile]
+                                                                  testFilePath]
                                  amend:NO
                            outputBlock:NULL
                                  error:NULL]) {
-      XCTFail(@"Commit with mesage 'new %@'", testFile);
+      XCTFail(@"Commit with mesage 'new %@'", testFilePath);
     }
   }
 
@@ -79,14 +83,10 @@
   [items enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
     XTHistoryItem *item = (XTHistoryItem *)obj;
 
-    if (idx != (items.count - 1)) {
-      XCTAssertTrue(item.lineInfo.numColumns == 1,
-                   @"%lu - incorrect numColumns=%lu", idx,
-                   item.lineInfo.numColumns);
+    if (idx == (items.count - 1)) {
+      XCTAssertEqual(item.lineInfo.numColumns, 0);
     } else {
-      XCTAssertTrue(item.lineInfo.numColumns == 0,
-                   @"%lu - incorrect numColumns=%lu", idx,
-                   item.lineInfo.numColumns);
+      XCTAssertEqual(item.lineInfo.numColumns, 1, "item %lu", idx);
     }
   }];
 }
