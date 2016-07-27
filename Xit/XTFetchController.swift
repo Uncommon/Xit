274a486ea0f4ddd2e2182f698815399cb9f02ceb
David Catmull <uncommon@uncommonplace.com>
2016-07-27 15:47:45 -0600

    Refactoring fetch and pull

--- Xit/XTFetchController.swift
+++ Xit/XTFetchController.swift
@@ -4,7 +4,7 @@ import Cocoa
 /// Runs a `fetch` operation.
 class XTFetchController: XTOperationController {
   
-  final func defaultRemoteName() -> String?
+  func defaultRemoteName() -> String?
   {
     let remotes = repository.remoteNames
     
@@ -36,13 +36,9 @@ class XTFetchController: XTOperationController {
     panel.pruneBranches = config.fetchPrune(panel.selectedRemote)
     self.windowController!.window!.beginSheet(panel.window!) { (response) in
       if response == NSModalResponseOK {
-        let pruneBranches = panel.pruneBranches
-            ? GTFetchPruneOption.Yes
-            : GTFetchPruneOption.No
-        
         self.executeFetch(panel.selectedRemote as String,
                           downloadTags: panel.downloadTags,
-                          pruneBranches: pruneBranches)
+                          pruneBranches: panel.pruneBranches)
       }
       else {
         self.ended()
@@ -50,92 +46,72 @@ class XTFetchController: XTOperationController {
     }
   }
   
-  final func ended()
+  func ended()
   {
     self.windowController?.fetchEnded()
   }
   
-  func credentialProvider() -> GTCredentialProvider
+  /// Fetch progress callback
+  func shouldStop(progress progress: git_transfer_progress) -> Bool
   {
-    return GTCredentialProvider() {
-        (type, url, user) -> GTCredential in
-      if checkCredentialType(type, flag: .SSHKey) {
-        return sshCredential(user) ?? GTCredential()
-      }
-      
-      guard checkCredentialType(type, flag: .UserPassPlaintext)
-      else { return GTCredential() }
-      
-      if let password = keychainPassword(url, user: user) {
-        do {
-          return try GTCredential(userName: user, password: password)
-        }
-        catch let error as NSError {
-          NSLog(error.description)
-        }
-      }
-      
-      guard let window = self.windowController?.window
-      else { return GTCredential() }
-      
-      let panel = XTPasswordPanelController.controller()
-      let semaphore = dispatch_semaphore_create(0)
-      var result: GTCredential?
-      
-      dispatch_async(dispatch_get_main_queue()) {
-        window.beginSheet(panel.window!) { (response) in
-          if response == NSModalResponseOK {
-            result = try? GTCredential(userName: panel.userName,
-                                       password: panel.password)
-          }
-          _ = dispatch_semaphore_signal(semaphore)
-        }
-      }
-      dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
-      return result ?? GTCredential()
+    if self.canceled {
+      return true
     }
+    
+    let progressValue =
+        progress.received_objects == progress.total_objects
+            ? -1.0
+            : Float(progress.total_objects) /
+              Float(progress.received_objects)
+    
+    XTStatusView.update(status: "Fetching",
+                        progress: progressValue,
+                        repository: repository)
+    return false
+  }
+  
+  /// User/password callback
+  func getPassword() -> (String, String)?
+  {
+    guard let window = self.windowController?.window
+    else { return nil }
+    
+    let panel = XTPasswordPanelController.controller()
+    let semaphore = dispatch_semaphore_create(0)
+    var result: (String, String)? = nil
+    
+    dispatch_async(dispatch_get_main_queue()) {
+      window.beginSheet(panel.window!) { (response) in
+        if response == NSModalResponseOK {
+          result = (panel.userName, panel.password)
+        }
+        _ = dispatch_semaphore_signal(semaphore)
+      }
+    }
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+    return result
   }
   
   func executeFetch(remoteName: String,
                     downloadTags: Bool,
-                    pruneBranches: GTFetchPruneOption)
+                    pruneBranches: Bool)
   {
     guard let remote = try? repository.remote(remoteName)
     else { return }
     
     XTStatusView.update(
-        status: "Fetching", progress: 0.0, repository: repository)
+        status: "Fetching...", progress: 0.0, repository: repository)
     
     let repo = repository  // For use in the block without being tied to self
     
     repo.executeOffMainThread {
       do {
-        let provider = self.credentialProvider()
-        let options: [String: AnyObject] = [
-            GTRepositoryRemoteOptionsDownloadTags: Int(downloadTags),
-            GTRepositoryRemoteOptionsFetchPrune: pruneBranches.rawValue,
-            GTRepositoryRemoteOptionsCredentialProvider: provider,
-            ]
-        
-        try repo.gtRepo.fetchRemote(remote, withOptions: options) {
-            (progress, stop) in
-          if self.canceled {
-            stop.memory = true
-          }
-          else {
-            let progressValue =
-                progress.memory.received_objects == progress.memory.total_objects
-                ? -1.0
-                : Float(progress.memory.total_objects) /
-                  Float(progress.memory.received_objects)
-            
-            XTStatusView.update(
-              status: "Fetching", progress: progressValue, repository: repo)
-          }
-        }
+        try repo.fetch(remote: remote,
+                       downloadTags: downloadTags,
+                       pruneBranches: pruneBranches,
+                       passwordBlock: self.getPassword,
+                       progressBlock: self.shouldStop)
         self.fetchCompleted()
-        XTStatusView.update(
-          status: "Fetch complete", progress: -1, repository: repo)
       }
       catch let error as NSError {
         dispatch_async(dispatch_get_main_queue()) {
@@ -154,53 +130,11 @@ class XTFetchController: XTOperationController {
     }
   }
   
-  func fetchCompleted() {}
-}
-
-
-func checkCredentialType(type: GTCredentialType,
-                         flag: GTCredentialType) -> Bool
-{
-  return (type.rawValue & flag.rawValue) != 0
-}
-
-func keychainPassword(urlString: String, user: String) -> String?
-{
-  guard let url = NSURL(string: urlString),
-        let server = url.host as NSString?
-  else { return nil }
-  
-  let user = user as NSString
-  var passwordLength: UInt32 = 0
-  var passwordData: UnsafeMutablePointer<Void> = nil
-  
-  let err = SecKeychainFindInternetPassword(
-      nil,
-      UInt32(server.length), server.UTF8String,
-      0, nil,
-      UInt32(user.length), user.UTF8String,
-      0, nil, 0,
-      .Any, .Default,
-      &passwordLength, &passwordData, nil)
-  
-  if err != noErr {
-    return nil
+  /// The fetch phase is complete. This is factored out so pull can override it.
+  func fetchCompleted()
+  {
+    XTStatusView.update(
+        status: "Fetch complete", progress: -1, repository: repository)
   }
-  return NSString(bytes: passwordData,
-                  length: Int(passwordLength),
-                  encoding: NSUTF8StringEncoding) as String?
 }
 
-func sshCredential(user: String) -> GTCredential?
-{
-  let publicPath =
-      ("~/.ssh/id_rsa.pub" as NSString).stringByExpandingTildeInPath
-  let privatePath =
-      ("~/.ssh/id_rsa" as NSString).stringByExpandingTildeInPath
-  
-  return try? GTCredential(
-      userName: user,
-      publicKeyURL: NSURL(fileURLWithPath: publicPath),
-      privateKeyURL: NSURL(fileURLWithPath: privatePath),
-      passphrase: "")
-}
--- Xit/XTRepository.swift
+++ Xit/XTRepository.swift
@@ -3,6 +3,66 @@ import Foundation
 
 extension XTRepository {
   
+  func credentialProvider(passwordBlock: () -> (String, String)?)
+      -> GTCredentialProvider
+  {
+    return GTCredentialProvider() {
+      (type, url, user) -> GTCredential in
+      if checkCredentialType(type, flag: .SSHKey) {
+        return sshCredential(user) ?? GTCredential()
+      }
+      
+      guard checkCredentialType(type, flag: .UserPassPlaintext)
+      else { return GTCredential() }
+      
+      if let password = keychainPassword(url, user: user) {
+        do {
+          return try GTCredential(userName: user, password: password)
+        }
+        catch let error as NSError {
+          NSLog(error.description)
+        }
+      }
+      
+      if let (userName, password) = passwordBlock(),
+         let result = try? GTCredential(userName: userName,
+                                        password: password) {
+        return result
+      }
+      return GTCredential()
+    }
+  }
+  
+  /// Initiates a fetch operation.
+  /// - parameter remote: The remote to pull from.
+  /// - parameter downloadTags: True to also download tags
+  /// - parameter pruneBranches: True to delete obsolete branch refs
+  /// - parameter passwordBlock: Callback for getting the user and password
+  /// - parameter progressBlock: Return true to stop the operation
+  public func fetch(remote remote: XTRemote,
+                    downloadTags: Bool,
+                    pruneBranches: Bool,
+                    passwordBlock: () -> (String, String)?,
+                    progressBlock: (git_transfer_progress) -> Bool) throws
+  {
+    let tagOption = downloadTags ? GTRemoteDownloadTagsAuto
+                                 : GTRemoteDownloadTagsNone
+    let pruneOption: GTFetchPruneOption = pruneBranches ? .Yes : .No
+    let pruneValue = NSNumber(long: pruneOption.rawValue)
+    let tagValue = NSNumber(unsignedInt: tagOption.rawValue)
+    let provider = self.credentialProvider(passwordBlock)
+    let options = [
+        GTRepositoryRemoteOptionsDownloadTags: tagValue,
+        GTRepositoryRemoteOptionsFetchPrune: pruneValue,
+        GTRepositoryRemoteOptionsCredentialProvider: provider]
+  
+    try gtRepo.fetchRemote(remote, withOptions: options) { (progress, stop) in
+      if progressBlock(progress.memory) {
+        stop.memory = true
+      }
+    }
+  }
+  
   /// Initiates pulling the given branch.
   /// - parameter branch: Either the local branch or the remote tracking branch.
   /// - parameter remote: The remote to pull from.
@@ -19,4 +79,53 @@ extension XTRepository {
       stop.memory = ObjCBool(progressBlock(progress.memory))
     }
   }
+}
+
+// MARK: Credential helpers
+
+func checkCredentialType(type: GTCredentialType,
+                         flag: GTCredentialType) -> Bool
+{
+  return (type.rawValue & flag.rawValue) != 0
+}
+
+func keychainPassword(urlString: String, user: String) -> String?
+{
+  guard let url = NSURL(string: urlString),
+        let server = url.host as NSString?
+  else { return nil }
+  
+  let user = user as NSString
+  var passwordLength: UInt32 = 0
+  var passwordData: UnsafeMutablePointer<Void> = nil
+  
+  let err = SecKeychainFindInternetPassword(
+      nil,
+      UInt32(server.length), server.UTF8String,
+      0, nil,
+      UInt32(user.length), user.UTF8String,
+      0, nil, 0,
+      .Any, .Default,
+      &passwordLength, &passwordData, nil)
+  
+  if err != noErr {
+    return nil
+  }
+  return NSString(bytes: passwordData,
+                  length: Int(passwordLength),
+                  encoding: NSUTF8StringEncoding) as String?
+}
+
+func sshCredential(user: String) -> GTCredential?
+{
+  let publicPath =
+      ("~/.ssh/id_rsa.pub" as NSString).stringByExpandingTildeInPath
+  let privatePath =
+      ("~/.ssh/id_rsa" as NSString).stringByExpandingTildeInPath
+  
+  return try? GTCredential(
+      userName: user,
+      publicKeyURL: NSURL(fileURLWithPath: publicPath),
+      privateKeyURL: NSURL(fileURLWithPath: privatePath),
+      passphrase: "")
 }
