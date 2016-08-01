824828665c97e709f4683de589c48aee91578fe7
David Catmull <uncommon@uncommonplace.com>
2016-08-01 13:47:38 -0600

    Some history progress

--- Xit/Commits.playground/Contents.swift
+++ Xit/Commits.playground/Contents.swift
@@ -1,13 +1,21 @@
 import Cocoa
 
+
 struct Commit {
   var sha: String
-  var parents: [Commit]
+  var parentSHAs: [String]
 }
+
+func == (left: Commit, right: Commit) -> Bool
+{
+  // No need to compare the parents
+  return left.sha == right.sha
+}
+
 
 class CommitEntry {
   let commit: Commit
-  var children: [Commit] = [Commit]()
+  var children = [Commit]()
   
   init(commit: Commit)
   {
@@ -15,43 +23,84 @@ class CommitEntry {
   }
 }
 
+func == (left: CommitEntry, right: CommitEntry) -> Bool
+{
+  return left.commit == right.commit
+}
+
+
+class Repository {
+  
+  func commit(forSHA sha: String) -> Commit?
+  {
+    return nil
+  }
+}
+
+
 class CommitHistory {
 
-  var commitLookup: [String: CommitEntry]
-  var entries: [CommitEntry]
+  let repository: Repository
+
+  var commitLookup = [String: CommitEntry]()
+  var entries = [CommitEntry]()
   
-  init()
+  init(repository: Repository)
   {
-    self.commitLookup = [String: CommitEntry]()
-    self.entries = [CommitEntry]()
+    self.repository = repository
   }
   
+  /// Adds a commit to the history, either at the end or after its
+  /// first parent.
+  /// - returns: false if the commit is already in the history.
   func add(commit: Commit, toParent parent: Commit?) -> Bool
   {
     guard commitLookup[commit.sha] == nil
     else { return false }
     
-    
     let entry = CommitEntry(commit: commit)
+    
     commitLookup[commit.sha] = entry
-    entries.append(entry) // actually insert it after the previous parent's commnits
+    
+    // Too do: find a more efficient way of finding the parent. Maybe
+    // keep a recently added list, since those would be likely candidates.
+    if let firstParentSHA = commit.parentSHAs.first,
+       let parentEntry = commitLookup[firstParentSHA],
+       let parentIndex = entries.indexOf({ $0 == parentEntry }) {
+      entries.insert(entry, atIndex: parentIndex+1)
+    }
+    else {
+      NSLog("Parent not found for \(commit.sha)")
+    }
+    entries.append(entry)
     return true;
   }
 
+  /// Adds a commit and all of its parents to the list.
   func process(startCommit: Commit, parent: Commit?)
   {
     var parent = parent
     var commit = startCommit
     var parentStack = [(Commit, Commit)]()
     
-    while add(commit, toParent: parent) && !commit.parents.isEmpty {
-      if commit.parents.count > 1 {
-        let parents = commit.parents[1..<commit.parents.count]
+    while add(commit, toParent: parent) && !commit.parentSHAs.isEmpty {
+      if commit.parentSHAs.count > 1 {
+        let parents = commit.parentSHAs[1..<commit.parentSHAs.count]
         
-        parentStack.appendContentsOf(parents.map({ return ($0, commit) }))
+        parentStack.appendContentsOf(parents.flatMap({
+          guard let parentCommit = self.repository.commit(forSHA: $0)
+          else { return nil }
+          return (commit, parentCommit)
+        }))
       }
-      parent = commit
-      commit = commit.parents[0]
+      if !commit.parentSHAs.isEmpty,
+         let next = repository.commit(forSHA: commit.parentSHAs[0]) {
+        parent = commit
+        commit = next
+      }
+      else {
+        break
+      }
     }
     for (commit, parent) in parentStack { // need to reverse order?
       process(commit, parent: parent)
