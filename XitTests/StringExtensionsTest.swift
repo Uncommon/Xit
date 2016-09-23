7bfcb5ef89e8aa891f37123258eff5c933d0b0c9
David Catmull <uncommon@uncommonplace.com>
2016-09-23 10:29:26 -0600

    Add tests for string extension methods

---
+++ XitTests/StringExtensionsTest.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Xit
+
+class StringExtensionsTest: XCTestCase
+{
+  func testPrefix()
+  {
+    XCTAssertEqual("none".stringByRemovingPrefix("1"), "none")
+    XCTAssertEqual("embiggen".stringByRemovingPrefix("em"), "biggen")
+  }
+  
+  func testSplitRefName()
+  {
+    XCTAssertNil("none".splitRefName())
+    
+    let (prefix, name) = "refs/heads/spunk".splitRefName()!
+    
+    XCTAssertEqual(prefix, "refs/heads/")
+    XCTAssertEqual(name, "spunk")
+  }
+}
