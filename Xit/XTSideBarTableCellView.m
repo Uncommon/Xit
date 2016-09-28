63c5af4a84d6e8f158b2d77d2c09198bb217ad86
David Catmull <uncommon@uncommonplace.com>
2016-09-28 16:03:46 -0600

    Adjust sidebar cell layout in drawRect instead of viewWillDraw
    Seems to fix the problem of status icons moving under the scroll bar

--- Xit/XTSideBarTableCellView.m
+++ Xit/XTSideBarTableCellView.m
@@ -6,18 +6,19 @@
 
 // The standard rowSizeStyle does some specific layout for us. To customize
 // layout for our button, we first call super and then modify things
-- (void)viewWillDraw
+- (void)drawRect:(NSRect)dirtyRect
 {
-  [super viewWillDraw];
   if (self.statusImage.image != nil) {
     NSRect textFrame = self.textField.frame;
     NSRect imageFrame = self.statusImage.frame;
-
+    
     imageFrame.origin.x = NSWidth(self.frame) - NSWidth(imageFrame) + 2;
     self.statusImage.frame = imageFrame;
     textFrame.size.width = NSMinX(imageFrame) - NSMinX(textFrame);
     self.textField.frame = textFrame;
   }
+
+  [super drawRect:dirtyRect];
 }
 
 @end
