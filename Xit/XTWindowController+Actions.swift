7737701b6425e00bb2ac5d8f9c992a0ea622db76
David Catmull <uncommon@uncommonplace.com>
2017-04-14 09:05:25 -0600

    Put window controller actions in their own file

---
+++ Xit/XTWindowController+Actions.swift
@@ -0,0 +1,123 @@
+import Foundation
+
+extension XTWindowController
+{
+  @IBAction func refresh(_ sender: AnyObject)
+  {
+    historyController.reload()
+    sidebarController.reload()
+  }
+  
+  @IBAction func showHideSidebar(_ sender: AnyObject)
+  {
+    let sidebarPane = mainSplitView.subviews[0]
+    let isCollapsed = sidebarHidden
+    let newWidth = isCollapsed
+                   ? savedSidebarWidth
+                   : mainSplitView.minPossiblePositionOfDivider(at: 0)
+    
+    if !isCollapsed {
+      savedSidebarWidth = sidebarPane.frame.size.width
+    }
+    mainSplitView.setPosition(newWidth, ofDividerAt: 0)
+    sidebarPane.isHidden = !isCollapsed
+    titleBarController!.viewControls.setSelected(sidebarHidden, forSegment: 0)
+  }
+  
+  @IBAction func showHideHistory(_ sender: AnyObject)
+  {
+    historyController.toggleHistory(sender)
+  }
+  
+  @IBAction func showHideDetails(_ sender: AnyObject)
+  {
+    historyController.toggleDetails(sender)
+  }
+  
+  @IBAction func verticalLayout(_ sender: AnyObject)
+  {
+    self.historyController.mainSplitView.isVertical = true
+    self.historyController.mainSplitView.adjustSubviews()
+  }
+  
+  @IBAction func horizontalLayout(_ sender: AnyObject)
+  {
+    self.historyController.mainSplitView.isVertical = false
+    self.historyController.mainSplitView.adjustSubviews()
+  }
+  
+  @IBAction func newTag(_: AnyObject)
+  {
+    _ = startOperation { XTNewTagController(windowController: self) }
+  }
+  
+  @IBAction func newBranch(_: AnyObject) {}
+  @IBAction func addRemote(_: AnyObject) {}
+
+  @IBAction func goBack(_: AnyObject)
+  {
+    withNavigating {
+      selectedModel.map { navForwardStack.append($0) }
+      selectedModel = navBackStack.popLast()
+    }
+  }
+  
+  @IBAction func goForward(_: AnyObject)
+  {
+    withNavigating {
+      selectedModel.map { navBackStack.append($0) }
+      selectedModel = navForwardStack.popLast()
+    }
+  }
+
+  // "Discardable let" is used here to specify the desired return type.
+  // swiftlint:disable redundant_discardable_let
+  @IBAction func fetch(_: AnyObject)
+  {
+    let _: XTFetchController? = startOperation()
+  }
+  
+  @IBAction func pull(_: AnyObject)
+  {
+    let _: XTPullController? = startOperation()
+  }
+  
+  @IBAction func push(_: AnyObject)
+  {
+    let _: XTPushController? = startOperation()
+  }
+  // swiftlint:enable redundant_discardable_let
+  
+  @IBAction func remoteSettings(_ sender: AnyObject)
+  {
+    guard let menuItem = sender as? NSMenuItem
+    else { return }
+    
+    let controller = XTRemoteOptionsController(windowController: self,
+                                               remote: menuItem.title)
+    
+    controller.start()
+  }
+  
+  @IBAction func viewSegmentClicked(_ sender: AnyObject)
+  {
+    switch (sender as! NSSegmentedControl).selectedSegment {
+      case 0:
+        showHideSidebar(sender)
+      default:
+        break
+    }
+  }
+}
+
+// MARK: Action helpers
+extension XTWindowController
+{
+  fileprivate func withNavigating(_ callback: () -> Void)
+  {
+    navigating = true
+    callback()
+    navigating = false
+    updateNavButtons()
+  }
+}
--- Xit/XTWindowController.swift
+++ Xit/XTWindowController.swift
@@ -131,14 +131,6 @@ class XTWindowController: NSWindowController, NSWindowDelegate,
     selectedModel = XTCommitChanges(repository: xtDocument!.repository, sha: sha)
   }
   
-  private func withNavigating(_ callback: () -> Void)
-  {
-    navigating = true
-    callback()
-    navigating = false
-    updateNavButtons()
-  }
-  
   func updateNavButtons()
   {
     updateNavControl(titleBarController?.navButtons)
@@ -183,89 +175,6 @@ class XTWindowController: NSWindowController, NSWindowDelegate,
         current: repo.currentBranch)
   }
   
-  @IBAction func refresh(_ sender: AnyObject)
-  {
-    historyController.reload()
-    sidebarController.reload()
-  }
-  
-  @IBAction func showHideSidebar(_ sender: AnyObject)
-  {
-    let sidebarPane = mainSplitView.subviews[0]
-    let isCollapsed = sidebarHidden
-    let newWidth = isCollapsed
-        ? savedSidebarWidth
-        : mainSplitView.minPossiblePositionOfDivider(at: 0)
-    
-    if !isCollapsed {
-      savedSidebarWidth = sidebarPane.frame.size.width
-    }
-    mainSplitView.setPosition(newWidth, ofDividerAt: 0)
-    sidebarPane.isHidden = !isCollapsed
-    titleBarController!.viewControls.setSelected(sidebarHidden, forSegment: 0)
-  }
-  
-  @IBAction func showHideHistory(_ sender: AnyObject)
-  {
-    historyController.toggleHistory(sender)
-  }
-  
-  @IBAction func showHideDetails(_ sender: AnyObject)
-  {
-    historyController.toggleDetails(sender)
-  }
-  
-  @IBAction func verticalLayout(_ sender: AnyObject)
-  {
-    self.historyController.mainSplitView.isVertical = true
-    self.historyController.mainSplitView.adjustSubviews()
-  }
-  
-  @IBAction func horizontalLayout(_ sender: AnyObject)
-  {
-    self.historyController.mainSplitView.isVertical = false
-    self.historyController.mainSplitView.adjustSubviews()
-  }
-  
-  @IBAction func newTag(_: AnyObject)
-  {
-    _ = startOperation { XTNewTagController(windowController: self) }
-  }
-  
-  @IBAction func newBranch(_: AnyObject) {}
-  @IBAction func addRemote(_: AnyObject) {}
-
-  @IBAction func goBack(_: AnyObject)
-  {
-    withNavigating {
-      selectedModel.map { navForwardStack.append($0) }
-      selectedModel = navBackStack.popLast()
-    }
-  }
-  
-  @IBAction func goForward(_: AnyObject)
-  {
-    withNavigating {
-      selectedModel.map { navBackStack.append($0) }
-      selectedModel = navForwardStack.popLast()
-    }
-  }
-
-  @IBAction func fetch(_: AnyObject)
-  {
-    let _: XTFetchController? = startOperation()
-  }
-  
-  @IBAction func pull(_: AnyObject)
-  {
-    let _: XTPullController? = startOperation()
-  }
-  
-  @IBAction func push(_: AnyObject)
-  {
-    let _: XTPushController? = startOperation()
-  }
-  
   public func startRenameBranch(_ branchName: String)
   {
     _ = startOperation { XTRenameBranchController(windowController: self,
@@ -297,33 +206,12 @@ class XTWindowController: NSWindowController, NSWindowDelegate,
     }
   }
   
-  @IBAction func viewSegmentClicked(_ sender: AnyObject)
-  {
-    switch (sender as! NSSegmentedControl).selectedSegment {
-      case 0:
-        showHideSidebar(sender)
-      default:
-        break
-    }
-  }
-  
   /// Called by the operation controller when it's done.
   func operationEnded(_ operation: XTOperationController)
   {
     if currentOperation == operation {
       currentOperation = nil
     }
-  }
-  
-  @IBAction func remoteSettings(_ sender: AnyObject)
-  {
-    guard let menuItem = sender as? NSMenuItem
-    else { return }
-    
-    let controller = XTRemoteOptionsController(windowController: self,
-                                               remote: menuItem.title)
-  
-    controller.start()
   }
   
   func updateRemotesMenu(_ menu: NSMenu)
