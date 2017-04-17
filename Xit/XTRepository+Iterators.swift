77b53df1bf78a9f7d9204aeddd2fb71302bc0cec
David Catmull <uncommon@uncommonplace.com>
2017-04-17 11:42:52 -0600

    Move repository iterator classes to another file

---
+++ Xit/XTRepository+Iterators.swift
@@ -0,0 +1,137 @@
+import Foundation
+
+extension XTRepository
+{
+  /// Branches is a sequence, not a collection, because the API does not provide
+  /// a count or indexed access.
+  struct Branches<BranchType: XTBranch>: Sequence
+  {
+    typealias Element = BranchType
+    let repo: XTRepository
+    let type: git_branch_t
+    
+    func makeIterator() -> BranchIterator<BranchType>
+    {
+      return BranchIterator<BranchType>(repo: repo, flags: type)
+    }
+  }
+
+  class BranchIterator<BranchType: XTBranch>: IteratorProtocol
+  {
+    let repo: XTRepository
+    let iterator: OpaquePointer?
+    
+    init(repo: XTRepository, flags: git_branch_t)
+    {
+      var result: OpaquePointer?
+      
+      if git_branch_iterator_new(&result,
+                                 repo.gtRepo.git_repository(), flags) == 0 {
+        self.iterator = result
+      }
+      else {
+        self.iterator = nil
+      }
+      self.repo = repo
+    }
+    
+    func next() -> BranchType?
+    {
+      guard let iterator = self.iterator
+        else { return nil }
+      
+      var type = git_branch_t(0)
+      var ref: OpaquePointer?
+      guard git_branch_next(&ref, &type, iterator) == 0,
+        ref != nil,
+        let gtRef = GTReference(gitReference: ref!,
+                                repository: repo.gtRepo),
+        let gtBranch = GTBranch(reference: gtRef,
+                                repository: repo.gtRepo)
+        else { return nil }
+      
+      return BranchType(gtBranch: gtBranch)
+    }
+    
+    deinit
+    {
+      git_branch_iterator_free(iterator)
+    }
+  }
+
+  /// The indexable collection of stashes in the repository.
+  class Stashes: Collection
+  {
+    typealias Iterator = StashIterator
+    
+    let repo: XTRepository
+    let refLog: OpaquePointer?
+    let count: Int
+    
+    static let stashRefName = "refs/stash"
+    
+    init(repo: XTRepository)
+    {
+      self.repo = repo
+      
+      let refLogPtr = UnsafeMutablePointer<OpaquePointer?>.allocate(capacity: 1)
+      guard git_reflog_read(refLogPtr, repo.gtRepo.git_repository(),
+                            Stashes.stashRefName) == 0
+        else {
+          self.refLog = nil
+          self.count = 0
+          return
+      }
+      
+      self.refLog = refLogPtr.pointee
+      self.count = git_reflog_entrycount(refLog)
+    }
+    
+    deinit
+    {
+      git_reflog_free(refLog)
+    }
+    
+    func makeIterator() -> StashIterator
+    {
+      return StashIterator(stashes: self)
+    }
+    
+    subscript(position: Int) -> XTStash
+    {
+      let entry = git_reflog_entry_byindex(refLog, position)
+      let message = String(cString: git_reflog_entry_message(entry))
+      
+      return XTStash(repo: repo, index: UInt(position), message: message)
+    }
+    
+    var startIndex: Int { return 0 }
+    var endIndex: Int { return count }
+    
+    func index(after i: Int) -> Int
+    {
+      return i + 1
+    }
+  }
+  
+  class StashIterator: IteratorProtocol
+  {
+    typealias Element = XTStash
+    let stashes: Stashes
+    var index: Int
+    
+    init(stashes: Stashes)
+    {
+      self.stashes = stashes
+      self.index = 0
+    }
+    
+    func next() -> XTStash?
+    {
+      let result = stashes[index]
+      
+      index += 1
+      return result
+    }
+  }
+}
