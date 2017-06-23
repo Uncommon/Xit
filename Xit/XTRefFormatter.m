40bb17a11166dfbfb886edf1736cd131f55d9eb5
David Catmull <uncommon@uncommonplace.com>
2017-06-23 13:51:06 -0600

    Use libgit2 to validate ref names

--- Xit/XTRefFormatter.m
+++ Xit/XTRefFormatter.m
@@ -1,4 +1,5 @@
 #import "XTRefFormatter.h"
+#include <git2.h>
 
 @implementation XTRefFormatter
 
@@ -32,69 +33,11 @@
 
 + (BOOL)isValidRefString:(NSString*)ref
 {
-  if ([ref length] == 0)
+  const char *cString = [ref cStringUsingEncoding:NSUTF8StringEncoding];
+
+  if (cString == NULL)
     return NO;
-  
-  // The rules, according to git help check-ref-format:
-
-  // They can include slash / for hierarchical (directory) grouping, but no
-  // slash-separated component can begin with a dot . or end with the
-  // sequence .lock.
-  if (([ref rangeOfString:@"/."].location != NSNotFound) ||
-      ([ref rangeOfString:@".lock/"].location != NSNotFound) ||
-      [ref hasPrefix:@".lock"])
-    return NO;
-
-  // They must contain at least one /. This enforces the presence of a
-  // category like heads/, tags/ etc. but the actual names are not
-  // restricted. If the --allow-onelevel option is used, this rule is
-  // waived.
-  // (We're effectively doing allow-onelevel here)
-
-  // They cannot have two consecutive dots .. anywhere.
-  if ([ref rangeOfString:@".."].location != NSNotFound)
-    return NO;
-
-  // They cannot have ASCII control characters (i.e. bytes whose values are
-  // lower than \040, or \177 DEL), space, tilde ~, caret ^, or colon :
-  // anywhere.
-  NSMutableCharacterSet *controlEtc =
-      [[NSCharacterSet controlCharacterSet] mutableCopy];
-
-  [controlEtc formUnionWithCharacterSet:
-      [NSCharacterSet characterSetWithCharactersInString:@" ~^:"]];
-  if ([ref rangeOfCharacterFromSet:controlEtc].location !=
-      NSNotFound)
-    return NO;
-
-  // They cannot have question-mark ?, asterisk *, or open bracket [
-  // anywhere.
-  NSCharacterSet *qab =
-      [NSCharacterSet characterSetWithCharactersInString:@"?*["];
-
-  if ([ref rangeOfCharacterFromSet:qab].location != NSNotFound)
-    return NO;
-
-  // They cannot begin or end with a slash / or contain multiple consecutive
-  // slashes
-  if ([ref hasPrefix:@"/"] ||
-      [ref hasSuffix:@"/"] ||
-      ([ref rangeOfString:@"//"].location != NSNotFound))
-    return NO;
-
-  // They cannot end with a dot ".".
-  if ([ref hasSuffix:@"."])
-    return NO;
-
-  // They cannot contain a sequence "@{".
-  if ([ref rangeOfString:@"@{"].location != NSNotFound)
-    return NO;
-  //
-  // They cannot contain a "\".
-  if ([ref rangeOfString:@"\\"].location != NSNotFound)
-    return NO;
-
-  return YES;
+  return git_reference_is_valid_name(cString);
 }
 
 @end
