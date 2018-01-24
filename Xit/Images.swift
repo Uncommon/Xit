9f3355a6ce57663ccfe646c7b0dda8cb7da3c706
David Catmull <davidcatmull@gmail.com>
2018-01-24 10:42:07 -0700

    New file list icons

--- Xit/Images.swift
+++ Xit/Images.swift
@@ -5,8 +5,11 @@ extension NSImage.Name
   static let xtBitBucketTemplate = NSImage.Name(rawValue: "bitbucketTemplate")
   static let xtBranchTemplate = NSImage.Name(rawValue: "branchTemplate")
   static let xtCloudTemplate = NSImage.Name(rawValue: "cloudTemplate")
+  static let xtFileTemplate = NSImage.Name(rawValue: "fileTemplate")
   static let xtFolderTemplate = NSImage.Name(rawValue: "folderTemplate")
   static let xtGitHubTemplate = NSImage.Name(rawValue: "githubTemplate")
+  static let xtRevertTemplate = NSImage.Name(rawValue: "revertTemplate")
+  static let xtStageAllTemplate = NSImage.Name(rawValue: "stageAllTemplate")
   static let xtStagingTemplate = NSImage.Name(rawValue: "stagingTemplate")
   static let xtStashTemplate = NSImage.Name(rawValue: "stashTemplate")
   static let xtSubmoduleTemplate = NSImage.Name(rawValue: "submoduleTemplate")
@@ -14,4 +17,5 @@ extension NSImage.Name
   static let xtTeamCityTemplate = NSImage.Name(rawValue: "teamcityTemplate")
   static let xtTracking = NSImage.Name(rawValue: "tracking")
   static let xtTrackingMissing = NSImage.Name(rawValue: "trackingMissing")
+  static let xtUnstageAllTemplate = NSImage.Name(rawValue: "unstageAllTemplate")
 }
