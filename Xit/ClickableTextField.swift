7e1469be987d6017fb6c4332d1668d905ea27e01
David Catmull <davidcatmull@gmail.com>
2018-06-12 11:58:37 -0600

    Parent links

---
+++ Xit/ClickableTextField.swift
@@ -0,0 +1,65 @@
+import Cocoa
+
+/// An NSTextField label that acts like a button, mainly to achieve
+/// ellipsis truncation which NSButton seems unable to do.
+class ClickableTextField: NSTextField
+{
+  var trackingTag: NSView.TrackingRectTag = 0
+  
+  init(title: String, target: AnyObject, action: Selector)
+  {
+    super.init(frame: .zero)
+    
+    stringValue = title
+    self.target = target
+    self.action = action
+    isEditable = false
+    isSelectable = false
+    isBezeled = false
+    drawsBackground = false
+    setHilited(false)
+    usesSingleLineMode = true
+    lineBreakMode = .byTruncatingTail
+    allowsExpansionToolTips = true
+    setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+  }
+  
+  required init?(coder: NSCoder)
+  {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func mouseDown(with event: NSEvent)
+  {
+    setHilited(true)
+    
+    trackingTag = addTrackingRect(bounds, owner: self, userData: nil,
+                                  assumeInside: true)
+  }
+  
+  override func mouseUp(with event: NSEvent)
+  {
+    setHilited(false)
+    if NSPointInRect(convert(event.locationInWindow, from: nil), bounds),
+       let action = self.action,
+       let target = self.target {
+      NSApp.sendAction(action, to: target, from: self)
+    }
+    removeTrackingRect(trackingTag)
+  }
+  
+  override func mouseEntered(with event: NSEvent)
+  {
+    setHilited(true)
+  }
+  
+  override func mouseExited(with event: NSEvent)
+  {
+    setHilited(false)
+  }
+  
+  func setHilited(_ hilited: Bool)
+  {
+    textColor = hilited ? .textColor : .linkColor
+  }
+}
--- Xit/CommitHeaderViewController.swift
+++ Xit/CommitHeaderViewController.swift
@@ -6,6 +6,9 @@ class CommitHeaderViewController: NSViewController
 {
   @IBOutlet weak var nameField: NSTextField!
   @IBOutlet weak var dateField: NSTextField!
+  @IBOutlet weak var parentsLabel: NSTextField!
+  @IBOutlet weak var parentsStack: NSStackView!
+  @IBOutlet weak var shaLabel: NSTextField!
   @IBOutlet weak var shaField: NSTextField!
   @IBOutlet weak var messageField: NSTextField!
   
@@ -24,25 +27,76 @@ class CommitHeaderViewController: NSViewController
   }
   weak var repository: CommitStorage!
   
-  
-  
   func loadHeader()
   {
     guard let commitSHA = commitSHA,
-          let commit = repository.commit(forSHA: commitSHA) as? XTCommit,
-          let scrollView = view.enclosingScrollView
-    else { return }
-    
-    view.scroll(NSPoint(x: 0,
-                        y: scrollView.bounds.size.height))
+          let commit = repository.commit(forSHA: commitSHA) as? XTCommit
+    else {
+      nameField.stringValue = "No selection"
+      parentsLabel.stringValue = ""
+      dateField.stringValue = ""
+      shaLabel.isHidden = true
+      messageField.stringValue = ""
+      return
+    }
     
     nameField.stringValue =
         "\(commit.authorName ?? "") <\(commit.authorEmail ?? "")"
     dateField.objectValue = commit.commitDate
     // separate name and date if author ≠ committer
+    shaLabel.isHidden = false
     shaField.stringValue = commitSHA
     messageField.stringValue = commit.message ?? ""
     
-    // set parents
+    while let subview = parentsStack.arrangedSubviews.first {
+      parentsStack.removeView(subview)
+    }
+    switch commit.parentOIDs.count {
+      case 0:
+        let noneLabel = NSTextField(labelWithString: "None")
+      
+        parentsStack.addArrangedSubview(noneLabel)
+      case 1:
+        parentsLabel.stringValue = "Parent:"
+        addParents(commit)
+      default:
+        parentsLabel.stringValue = "Parents:"
+        addParents(commit)
+    }
+    
+    view.needsLayout = true
+    
+    if let scrollView = view.enclosingScrollView {
+      view.scroll(NSPoint(x: 0,
+                          y: scrollView.bounds.size.height))
+    }
+  }
+  
+  func addParents(_ commit: Commit)
+  {
+    for (index, oid) in commit.parentOIDs.enumerated() {
+      guard let parentCommit = repository.commit(forOID: oid)
+      else { continue }
+      let message = parentCommit.messageSummary
+      
+      let button = ClickableTextField(title: message, target: self,
+                                      action: #selector(chooseParent(_:)))
+      
+      button.tag = index
+      parentsStack.addArrangedSubview(button)
+    }
+  }
+  
+  @IBAction
+  func chooseParent(_ sender: Any?)
+  {
+    guard let commitSHA = commitSHA,
+          let commit = repository.commit(forSHA: commitSHA) as? XTCommit,
+          let control = sender as? NSControl
+    else { return }
+    
+    let parentOID = commit.parentOIDs[control.tag]
+    
+    repoController.select(sha: parentOID.sha)
   }
 }
