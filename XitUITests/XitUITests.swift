e79e3af1184446def7fe69c5122e6bb17eebc224
David Catmull <davidcatmull@gmail.com>
2019-11-06 10:55:32 -0700

    Check commit header content

--- XitUITests/XitUITests.swift
+++ XitUITests/XitUITests.swift
@@ -45,13 +45,30 @@ class XitUITests: XCTestCase
     let rows = app.outlines["commitFiles"].outlineRows
     
     for (index, name) in names.enumerated() {
-      let label = rows.element(boundBy: index).staticTexts.firstMatch.value
-                  as? String ?? ""
+      let label = rows.element(boundBy: index).staticTexts.firstMatch.stringValue
       
       XCTAssertEqual(label, name)
     }
   }
   
+  func assertCommitHeader(date: String, sha: String, name: String,
+                          parents: [String], message: String)
+  {
+    let header = app.otherElements["commitInfo"].firstMatch
+    let dateField = header.staticTexts["date"].firstMatch
+    let shaField = header.staticTexts["sha"].firstMatch
+    let nameField = header.staticTexts["name"].firstMatch
+    let messageField = header.staticTexts["message"].firstMatch
+    let parentFields = header.otherElements["parents"]
+                             .staticTexts.allElementsBoundByAccessibilityElement
+    
+    XCTAssertEqual(dateField.stringValue, date)
+    XCTAssertEqual(shaField.stringValue, sha)
+    XCTAssertEqual(nameField.stringValue, name)
+    XCTAssertEqual(parentFields.map { $0.stringValue }, parents)
+    XCTAssertEqual(messageField.stringValue, message)
+  }
+
   func testRepoWindow()
   {
     guard repoURL != nil else { return }
@@ -77,22 +94,22 @@ class XitUITests: XCTestCase
     
     // staging has 1 > 0
     
-    assertBranches([
-        "1-and_more",
-        "and-how",
-        "andhow-ad",
-        "asdf",
-        "blah",
-        "feature",
-        "hi!",
-        "master",
-        "new",
-        "other-branch",
-        "wat",
-        "whateelse",
-        "whup",
-        ])
+    assertBranches(["1-and_more", "and-how", "andhow-ad", "asdf", "blah",
+                    "feature", "hi!", "master", "new", "other-branch",
+                    "wat", "whateelse", "whup",
+                    ])
     
     assertCommitFiles(["README.md", "hero_slide1.png", "jquery-1.8.1.min.js"])
+    
+    assertCommitHeader(date: "Jan 10, 2013 at 7:11 AM",
+                       sha: "a4bca6b67a5483169963572ee3da563da33712f7",
+                       name: "Danny Greg <danny@github.com>",
+                       parents: ["Rename README."],
+                       message: "Add 2 text and 1 binary file for diff tests.")
   }
+}
+
+extension XCUIElement
+{
+  var stringValue: String { value as? String ?? "" }
 }
