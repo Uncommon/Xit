96bd188fe180724dd90a4da49f951119309dc0d9
David Catmull <davidcatmull@gmail.com>
2021-02-24 11:39:37 -0700

    Split out split view stuff

--- Xit/FileView/FileViewController.swift
+++ Xit/FileView/FileViewController.swift
@@ -230,7 +230,8 @@ class FileViewController: NSViewController, RepositoryWindowViewController
     diffController.stagingDelegate = self
   }
 
-  override func viewWillAppear() {
+  override func viewWillAppear()
+  {
     super.viewWillAppear()
 
     for listController in allListControllers {
@@ -452,82 +453,6 @@ class FileViewController: NSViewController, RepositoryWindowViewController
     }
     catch {
       NSLog("Unexpected revert error")
-    }
-  }
-}
-
-// MARK: NSSplitViewDelegate
-extension FileViewController: NSSplitViewDelegate
-{
-  static let minHeaderHeight: CGFloat = 45
-  static let minDetailHeight: CGFloat = 60
-
-  var maxHeaderHeight: CGFloat
-  {
-    headerSplitView.bounds.height - Self.minDetailHeight
-  }
-
-  public func splitView(_ splitView: NSSplitView,
-                        shouldAdjustSizeOfSubview view: NSView) -> Bool
-  {
-    // Supposedly this can be done with holding priorities
-    // but that's not working.
-    switch splitView {
-      case headerSplitView:
-        return view != headerTabView
-      case fileSplitView:
-        return view != fileListTabView
-      default:
-        return true
-    }
-  }
-
-  public func splitView(_ splitView: NSSplitView,
-                        constrainMinCoordinate proposedMinimumPosition: CGFloat,
-                        ofSubviewAt dividerIndex: Int) -> CGFloat
-  {
-    switch splitView {
-      case headerSplitView:
-        return Self.minHeaderHeight
-      default:
-        return proposedMinimumPosition
-    }
-  }
-
-  public func splitView(_ splitView: NSSplitView,
-                        constrainMaxCoordinate proposedMaximumPosition: CGFloat,
-                        ofSubviewAt dividerIndex: Int) -> CGFloat
-  {
-    switch splitView {
-      case headerSplitView:
-        return maxHeaderHeight
-      default:
-        return proposedMaximumPosition
-    }
-  }
-
-  public func splitViewDidResizeSubviews(_ notification: Notification)
-  {
-    guard !resizeRecursing,
-          let splitView = notification.object as? NSSplitView,
-          splitView == headerSplitView
-    else { return }
-    let headerHeight = splitView.arrangedSubviews[0].bounds.height
-
-    resizeRecursing = true
-    defer {
-      resizeRecursing = false
-    }
-
-    if headerHeight < Self.minHeaderHeight {
-      splitView.setPosition(Self.minHeaderHeight, ofDividerAt: 0)
-    }
-    else {
-      let maxHeaderHeight = self.maxHeaderHeight
-
-      if headerHeight > maxHeaderHeight {
-        splitView.setPosition(maxHeaderHeight, ofDividerAt: 0)
-      }
     }
   }
 }
