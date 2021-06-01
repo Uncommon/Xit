4959a726e13a4a6a8cad245a505df670819765c8
David Catmull <davidcatmull@gmail.com>
2021-05-31 22:33:17 -0600

    Add drag and drop support

--- Xit/Utils/PathField.swift
+++ Xit/Utils/PathField.swift
@@ -3,17 +3,62 @@ import SwiftUI
 struct PathField: View
 {
   @Binding var path: String
+  @State var isDropTarget: Bool = false
+  
+  struct FolderDropDelegate: DropDelegate
+  {
+    @Binding var path: String
+    @Binding var isDropTarget: Bool
+    
+    func dropEntered(info: DropInfo)
+    {
+      isDropTarget = true
+    }
+    
+    func dropExited(info: DropInfo)
+    {
+      isDropTarget = false
+    }
+
+    func performDrop(info: DropInfo) -> Bool
+    {
+      isDropTarget = false
+      
+      guard let provider = info.itemProviders(for: [.fileURL]).first
+      else { return false }
+      
+      _ = provider.loadObject(ofClass: URL.self) {
+        (url, error) in
+        // It seems the only way to verify that the dropped item is a directory
+        // is to examine the data asynchronously, so dropped files are ignored
+        // instead of rejected.
+        var isDirectory: ObjCBool = false
+        guard let url = url,
+              FileManager.default.fileExists(atPath: url.path,
+                                             isDirectory: &isDirectory),
+              isDirectory.boolValue
+        else { return }
+        
+        path = url.path
+      }
+      return true
+    }
+  }
   
   var body: some View
   {
     HStack {
       TextField("", text: $path)
+        .overlay(Color.clear.border(Color(NSColor.selectedControlColor),
+                                    width: isDropTarget ? 2 : 0))
       Button {
         chooseFolder()
       } label: {
         Image(systemName: "folder")
       }.buttonStyle(BorderlessButtonStyle())
-    }
+    }.onDrop(of: [.fileURL],
+             delegate: FolderDropDelegate(path: $path,
+                                          isDropTarget: $isDropTarget))
   }
   
   func findParentWindow() -> NSWindow?
@@ -56,6 +101,6 @@ struct PathField_Previews: PreviewProvider
   
   static var previews: some View
   {
-    Preview()
+    Preview().padding()
   }
 }
