fa96c9091b6f81410bbd70a4b977ea3f03043aae
David Catmull <davidcatmull@gmail.com>
2021-07-13 11:49:46 -0600

    Start clean panel

---
+++ Xit/Operations/CleanPanel.swift
@@ -0,0 +1,105 @@
+import SwiftUI
+
+struct CleanableItem
+{
+  enum ItemType { case file, directory }
+
+  let path: String
+  let type: ItemType
+  let ignored: Bool
+
+  var icon: NSImage
+  {
+    switch type {
+      case .file:
+        return NSWorkspace.shared.icon(forFileType: path.pathExtension)
+      case .directory:
+        return .init(named: NSImage.folderName)!
+    }
+  }
+}
+
+extension CleanableItem: Identifiable
+{
+  var id: String { path }
+}
+
+struct CleanPanel: View
+{
+  @Binding var cleanFolders: Bool
+  @Binding var cleanIgnored: Bool
+  @Binding var cleanNonIgnored: Bool
+  @Binding var regex: String
+  @Binding var items: [CleanableItem]
+
+  @State var selection: Set<String> = []
+
+  var filteredItems: [CleanableItem]
+  {
+    items.filter {
+      $0.ignored && cleanIgnored || !$0.ignored && cleanNonIgnored
+    }
+  }
+
+  var body: some View
+  {
+    VStack(alignment: .leading) {
+      Toggle("Clean untracked directories", isOn: $cleanFolders)
+      Toggle("Clean ignored files", isOn: $cleanIgnored)
+      Toggle("Clean non-ignored files", isOn: $cleanNonIgnored)
+      HStack {
+        Text("Regex for non-ignored files:")
+          .foregroundColor(cleanNonIgnored ? .primary : .secondary)
+        TextField("", text: $regex)
+      }.disabled(!cleanNonIgnored)
+      List(filteredItems, selection: $selection) { item in
+        HStack {
+          Image(nsImage: item.icon)
+            .resizable().frame(width: 16, height: 16)
+          Text(item.path)
+        }
+      }.border(Color(.separatorColor))
+      HStack {
+        Spacer()
+        Button("Cancel") {
+
+        }.keyboardShortcut(.cancelAction)
+        Button("Clean Selected") {
+
+        }.disabled(selection.isEmpty)
+        Button("Clean All") {
+          // clean
+        }.keyboardShortcut(.defaultAction).disabled(filteredItems.isEmpty)
+      }
+    }.padding()
+  }
+}
+
+struct CleanPanel_Previews: PreviewProvider
+{
+  struct Preview: View
+  {
+    @State var cleanFolders = true
+    @State var cleanIgnored = true
+    @State var cleanNonIgnored = true
+    @State var regex = ""
+
+    @State var items: [CleanableItem] = [
+      .init(path: "build.o", type: .file, ignored: true),
+      .init(path: "file.txt", type: .file, ignored: false),
+      .init(path: "folder", type: .directory, ignored: true),
+    ]
+
+    var body: some View
+    {
+      CleanPanel(cleanFolders: $cleanFolders, cleanIgnored: $cleanIgnored,
+                 cleanNonIgnored: $cleanNonIgnored, regex: $regex,
+                 items: $items)
+    }
+  }
+
+  static var previews: some View
+  {
+    Preview()
+  }
+}
