25b5ef91c667d1af7a2b325b46e2a3e15b897afc
David Catmull <davidcatmull@gmail.com>
2024-06-25 14:14:38 -0600

    WIP Basic authentication in a separate object

--- Xit/Services/BasicAuthService.swift
+++ Xit/Services/BasicAuthService.swift
@@ -1,9 +1,106 @@
 import Foundation
+import Combine
 import Siesta
 
 extension Notification.Name
 {
   static let authenticationStatusChanged = Self("AuthStatusChanged")
+}
+
+protocol BasicAuthenticatorDelegate: AnyObject
+{
+  func didAuthenticate(responseResource: Resource)
+}
+
+protocol Authentication
+{
+  var authenticationStatusPublisher: any Publisher<Services.Status, Never> { get }
+}
+
+class BasicAuthenticator: ObservableObject
+{
+  let account: Account
+  @Published var authenticationStatus: Services.Status
+  var password: String
+  private let authenticationPath: String
+  weak var delegate: BasicAuthenticatorDelegate?
+
+  init?(account: Account, password: String, authenticationPath: String)
+  {
+    self.account = account
+    self.password = password
+    self.authenticationStatus = .notStarted
+    self.authenticationPath = authenticationPath
+  }
+
+  func configure(service: Service)
+  {
+    service.configure {
+      (builder) in
+      if let data = "\(self.account.user):\(self.password)"
+        .data(using: String.Encoding.utf8)?
+        .base64EncodedString(options: []) {
+        builder.headers["Authorization"] = "Basic \(data)"
+      }
+      else {
+        serviceLogger.debug("""
+        Couldn't construct auth header for \
+        \(self.account.user) @ \(service.baseURL?.absoluteString ?? "?")
+        """)
+      }
+    }
+  }
+
+  /// Checks that the user and password are accepted by the server.
+  func attemptAuthentication(service: Service, path: String? = nil)
+  {
+    objc_sync_enter(self)
+    defer { objc_sync_exit(self) }
+
+    authenticationStatus = .inProgress
+
+    let path = path ?? authenticationPath
+    let authResource = service.resource(path)
+
+    for request in authResource.allRequests {
+      request.cancel()
+    }
+    authResource.addObserver(owner: self) {
+      [self] (resource, event) in
+      switch event {
+
+        case .newData, .notModified:
+          authenticationStatus = .done
+          delegate?.didAuthenticate(responseResource: resource)
+
+        case .error:
+          guard let error = resource.latestError
+          else {
+            serviceLogger.debug("Error event with no error")
+            authenticationStatus = .failed(nil)
+            return
+          }
+
+          authenticationStatus =
+              (error.cause is Siesta.RequestError.Cause.RequestCancelled)
+              ? .notStarted
+              : .failed(error)
+
+        default:
+          break
+      }
+    }
+    // Use a custom request to skip the XML transformer
+    _ = authResource.load(using: authResource.request(.get))
+  }
+}
+
+extension BasicAuthenticator: Authentication
+{
+  var authenticationStatusPublisher: any Publisher<Services.Status, Never>
+  {
+    $authenticationStatus
+  }
 }
 
 /// Abstract service class that handles HTTP basic authentication.
