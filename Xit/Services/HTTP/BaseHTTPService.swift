29b7ab3ad6dd2337503bef88a71331c30dde020c
David Catmull <davidcatmull@gmail.com>
2026-02-17 08:43:23 -0700

    Whitespace fixes

--- Xit/Services/HTTP/BaseHTTPService.swift
+++ Xit/Services/HTTP/BaseHTTPService.swift
@@ -9,17 +9,17 @@ class BaseHTTPService: ObservableObject, Identifiable
   // lists. There may be a better way, like having an explicit, more meaningful
   // identifier.
   let id = UUID()
-
+  
   // Maybe this should be immutable, and we recreate the service object for a new account
   var account: Account
   let networkService: NetworkService
   var authProvider: BasicAuthProvider
   let passwordStorage: any PasswordStorage
-
+  
   private let authenticationPath: String
-
+  
   @Published var authenticationStatus: Services.Status = .notStarted
-
+  
   init(account: Account,
        password: String,
        passwordStorage: any PasswordStorage = KeychainStorage.shared,
@@ -29,13 +29,13 @@ class BaseHTTPService: ObservableObject, Identifiable
     self.account = account
     self.passwordStorage = passwordStorage
     self.authenticationPath = authenticationPath
-
+    
     self.authProvider = BasicAuthProvider(username: account.user,
                                           password: password)
-
+    
     // Create network configuration with sensible defaults
     let configuration = NetworkConfiguration(headers: [:])
-
+    
     // If network service is provided (e.g. for testing), use it.
     // Otherwise create a standard URLSessionNetworkService with our auth provider.
     if let providedService = networkService {
@@ -50,7 +50,7 @@ class BaseHTTPService: ObservableObject, Identifiable
       )
     }
   }
-
+  
   /// Re-generates the authentication header with the new credentials.
   /// Returns 'true' on success, 'false' if credentials are invalid.
   func updateAuthentication(_ user: String, password: String) -> Bool
@@ -89,28 +89,28 @@ class BaseHTTPService: ObservableObject, Identifiable
 
     return true
   }
-
+  
   /// Checks that the user and password are accepted by the server.
   func attemptAuthentication(path: String? = nil) async
   {
     let path = path ?? authenticationPath
-
+    
     await MainActor.run {
       self.authenticationStatus = .inProgress
     }
-
+    
     do {
       let endpoint = Endpoint(baseURL: account.location,
                               path: path,
                               method: .get)
-
+      
       // We don't care about the response body, just that it succeeds (200 OK)
       let data: Data = try await networkService.request(endpoint)
-
+      
       await MainActor.run {
         self.authenticationStatus = .done
       }
-
+      
       await didAuthenticate(data: data)
     }
     catch {
@@ -128,10 +128,10 @@ class BaseHTTPService: ObservableObject, Identifiable
       }
     }
   }
-
+  
   /// Hook for subclasses to process successful authentication response
   func didAuthenticate(data: Data) async {}
-
+  
   /// Updates the account and attempts re-authentication
   func accountUpdated(oldAccount: Account, newAccount: Account)
   {
@@ -144,9 +144,9 @@ class BaseHTTPService: ObservableObject, Identifiable
       authenticationStatus = .unknown
       return
     }
-
+    
     self.account = newAccount
-
+    
     if updateAuthentication(newAccount.user, password: password) {
       Task {
         await attemptAuthentication()
